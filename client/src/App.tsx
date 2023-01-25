01cdc26002c87893fdf2e9990be43450b8546418
DESKTOP-R5CTNNO\IT-Sparsha <Sukum.p@sparsha.com>
2023-01-25 17:03:42 +0700

    Connect API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+
 
 function App() {
+
+  const [data,setData] = useState<any[]>([])
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/products")
+    .then(res => res.json())
+    .then(jsn => setData(jsn))
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+        {data.map(x => (
+          <li key={x.id}>{x.name} - {x.price}</li>
+        ))}
+
+    </ul>
   );
 }
 
