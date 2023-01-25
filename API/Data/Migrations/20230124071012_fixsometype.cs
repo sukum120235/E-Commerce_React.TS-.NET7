71daa7f1d13fe922ae8966b64ab349617b0db039
DESKTOP-R5CTNNO\IT-Sparsha <Sukum.p@sparsha.com>
2023-01-25 10:48:35 +0700

    Upload Project

---
+++ API/Data/Migrations/20230124071012_fixsometype.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixsometype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PictureURL",
+                table: "Products",
+                newName: "PictureUrl");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuantityInStock",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "Products",
+                newName: "PictureURL");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuantityInStock",
+                table: "Products",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
